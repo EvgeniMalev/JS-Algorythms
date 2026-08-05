a0872dd0df13fbd7077c17ac15e673165c407912
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2026-08-05 22:38:37 +0300

    Create factoriel.js

---
+++ Recursion/factoriel.js
@@ -0,0 +1,10 @@
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
+
+let number = 5;
+console.log("Факториел на " + number + " = " + factorial(number));
